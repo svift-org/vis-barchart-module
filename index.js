1e661f4243238284a25a2ce9e68c8edbf639a9be
sebastian-meier <contact@sebastianmeier.eu>
2017-07-15 02:20:53 +0200

    bugfixing

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-SVIFT.vis.barchart = (function (data, root) {
+SVIFT.vis.barchart = (function (data, container) {
  
   // Module object
-  var module = SVIFT.vis.base(data, root);
+  var module = SVIFT.vis.base(data, container);
  
   module.setup = function () {
-    module.svg.append('rect')
+    module.g.append('rect')
       .attr('x', module.config.margin.left)
       .attr('y', module.config.margin.top)
       .style('stroke','#000')
       .style('fill','transparent');
 
-    module.svg.append('text')
+    module.g.append('text')
       .style('fill','#000')
       .attr('dx',50)
       .attr('dy',50)
@@ -18,9 +18,9 @@ SVIFT.vis.barchart = (function (data, root) {
   };
 
   module.resize = function () {
-    var bb = module.root.node().getBBox();
+    var bb = module.svg.node().getBBox();
     console.log(bb);
-    module.svg.select('rect')
+    module.g.select('rect')
       .attr('width', bb.width)
       .attr('height', bb.height);
   };
