2e3035ed9382fa5f327faaa166bcb0ffeb08e3e5
sebastian-meier <contact@sebastianmeier.eu>
2017-07-15 02:35:55 +0200

    bugfixing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ SVIFT.vis.barchart = (function (data, container) {
   };
 
   module.resize = function () {
-    console.log(module.container.nodes()[0])
+    console.log(module.container.nodes()[0].getBBox())
     var bb = module.container.nodes()[0].getBBox();
     module.g.select('rect')
       .attr('width', bb.width)
