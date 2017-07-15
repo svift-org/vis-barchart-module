18370e53fb7fdafde67616eebbb442afa760c4b8
sebastian-meier <contact@sebastianmeier.eu>
2017-07-15 02:04:02 +0200

    bugfixing

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ SVIFT.vis.barchart = (function (data, root) {
   // Module object
   var module = SVIFT.vis.base(data, root);
  
+  module.setup = function () {
+    module.svg.append('rect');
+    module.svg.append('text')
+      .attr('dx',50)
+      .attr('dy',50)
+      .text('Hello World');
+  };
+
+  module.resize = function () {
+    var bb = model.svg.node().getBBox();
+    module.svg.select('rect')
+      .attr('width', bb.width)
+      .attr('height', bb.height);
+  };
+
   return module;
  });
