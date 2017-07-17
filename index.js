9fdc8ef1fb2d26d9ddbf2934dfefea99f90627e6
sebastian-meier <contact@sebastianmeier.eu>
2017-07-17 15:23:32 +0200

    framebased animation

--- index.js
+++ index.js
@@ -3,24 +3,29 @@ SVIFT.vis.barchart = (function (data, container) {
   // Module object
   var module = SVIFT.vis.base(data, container);
  
+  module.timeline [
+    rect: {start:0, end:5000, func:module.drawRect, obj:{ease:d3.easeCubicInOut, interpolate:null}}
+  ];
+
   module.setup = function () {
     module.g.append('rect')
+      .attr('width', 50)
+      .attr('height', 50)
+      .attr('y', 50)
+      .attr('x', 50)
       .style('stroke','#000')
       .style('fill','transparent');
-
-    module.g.append('text')
-      .style('fill','#000')
-      .attr('dx',50)
-      .attr('dy',50)
-      .text('Hello World');
   };
 
   module.resize = function () {
     var width = module.container.node().offsetWidth,
       height = module.container.node().offsetHeight;
-    module.g.select('rect')
-      .attr('width', width-(module.config.margin.top+module.config.margin.bottom))
-      .attr('height', height-(module.config.margin.left+module.config.margin.right));
+
+    module.timeline.rect.obj.interpolate = d3.interpolate(50, width-100);
+  };
+
+  module.drawRect = function(t){
+    module.g.select('rect').attr('x', module.timeline.rect.obj.interpolate(module.timeline.rect.obj.ease(t)));
   };
 
   return module;
