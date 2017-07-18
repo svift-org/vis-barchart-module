59903cb2ea7783e364ecaf035deae18c8870e878
sebastian-meier <contact@sebastianmeier.eu>
2017-07-19 00:44:11 +0200

    bugfixes

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ SVIFT.vis.barchart = (function (data, container) {
     module.d3config.bars = module.g.append('g').selectAll('rect').data(data.data.data).enter().append('rect')
       .attr('x', module.d3config.axisWidth)
       .style('stroke','transparent')
-      .style('fill','#000');
+      .style('fill',function(d){
+        return 'rgb('+Math.random()*255+','+Math.random()*255+','+Math.random()*255+')';
+      });
   };
 
   module.resize = function () {
